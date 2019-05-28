28d7248fe228c588e889fc6c40dd4d22a72fff2b
John Judd <john.judd@c2fo.com>
2019-05-28 00:59:34 -0500

    Update doc and utils lib for validation

--- backend/testsuite/backend_integration_test.go
+++ backend/testsuite/backend_integration_test.go
@@ -11,12 +11,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/c2fo/vfs/v3"
-	"github.com/c2fo/vfs/v3/backend/gs"
-	_os "github.com/c2fo/vfs/v3/backend/os"
-	"github.com/c2fo/vfs/v3/backend/s3"
-	"github.com/c2fo/vfs/v3/utils"
-	"github.com/c2fo/vfs/v3/vfssimple"
+	"github.com/c2fo/vfs/v4"
+	"github.com/c2fo/vfs/v4/backend/gs"
+	_os "github.com/c2fo/vfs/v4/backend/os"
+	"github.com/c2fo/vfs/v4/backend/s3"
+	"github.com/c2fo/vfs/v4/utils"
+	"github.com/c2fo/vfs/v4/vfssimple"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -74,21 +74,20 @@ func (s *vfsTestSuite) TestScheme() {
 func (s *vfsTestSuite) FileSystem(baseLoc vfs.Location) {
 	//setup filesystem
 	fs := baseLoc.FileSystem()
-	/*
-		NewFile initializes a File on the specified volume at path 'name'. On error, nil is returned
-		for the file.
-
-		"volume" my be an empty string for filesystems that don't have a volume or
-		Note that not all filesystems will have a "volume" and will therefore be "":
-		file:///path/to/file has a volume of "" and name /path/to/file
-		whereas
-		s3://mybucket/path/to/file has a volume of "mybucket
-
-		path is expected to always be absolute and therefore must begin with a separator character and may not be an
-		empty string.
-
-		NewFile(volume string, name string) (File, error)
-	*/
+	// NewFile initializes a File on the specified volume at path 'name'. On error, nil is returned for the file.
+	//
+	//   * path is expected to always be absolute and therefore must begin with a separator character and may not be an
+	//   empty string.  As a path to a file, 'name' may not end with a trailing separator character.
+	//   * The file may or may not already exist.
+	//   * Upon success, a vfs.File, representing the file's new path (location path + file relative path), will be returned.
+	//   * On error, nil is returned for the file.
+	//   * fileName param must be a an absolute path to a file and therefore may not start or end with a separator characters.
+	//     This is not to be confused with vfs.Locations' NewFile() which requires a path relative the current location.
+	//   * Note that not all filesystems will have a "volume" and will therefore be "":
+	//       file:///path/to/file has a volume of "" and name /path/to/file
+	//       whereas
+	//       s3://mybucket/path/to/file has a volume of "mybucket and name /path/to/file
+	//       results in /tmp/dir1/newerdir/file.txt for the final vfs.File path.
 	filepaths := map[string]bool{
 		"/path/to/file.txt":    true,
 		"/path/./to/file.txt":  true,
@@ -109,17 +108,14 @@ func (s *vfsTestSuite) FileSystem(baseLoc vfs.Location) {
 		}
 	}
 
-	/*
-		NewLocation initializes a Location on the specified volume with the given path. On error, nil is returned
-		for the location
-
-		* path must end in separator character
-		* path is expected always be absolute and therefore must begin with a separator character
-
-		See NewFile for note on volume.
-
-		NewLocation(volume string, path string) (Location, error)
-	*/
+	// NewLocation initializes a Location on the specified volume with the given path. On error, nil is returned
+	// for the location
+	//
+	//   * The path may or may not already exist.  Note that on keystore filesystems like S3 or GCS, paths never truly exist.
+	//   * path is expected to always be absolute and therefore must begin and end with a separator character. This is not to
+	//        be confused with vfs.Locations' NewLocation() which requires a path relative to the current location.
+	//
+	// See NewFile for note on volume.
 	locpaths := map[string]bool{
 		"/path/to/":         true,
 		"/path/./to/":       true,
@@ -204,113 +200,82 @@ func (s *vfsTestSuite) Location(baseLoc vfs.Location) {
 		s.NoError(err)
 		s.Equal(rootTestLoc+"this/", l.URI())
 	*/
-	/*
-		String() must be implemented to satisfy the stringer interface.  This ends up simply calling URI().
 
-		fmt.Stringer
-	*/
+	// List returns a slice of strings representing the base names of the files found at the Location. All implementations
+	// are expected to return ([]string{}, nil) in the case of a non-existent directory/prefix/location. If the user
+	// cares about the distinction between an empty location and a non-existent one, Location.Exists() should be checked
+	// first.
+	//
+	//	====		List() ([]string, error)
 
-	/*
-				List returns a slice of strings representing the base names of the files found at the Location. All implementations
-				are expected to return ([]string{}, nil) in the case of a non-existent directory/prefix/location. If the user
-				cares about the distinction between an empty location and a non-existent one, Location.Exists() should be checked
-				first.
+	// ListByPrefix returns a slice of strings representing the base names of the files found in Location whose
+	// filenames match the given prefix. An empty slice will be returned even for locations that don't exist.
+	//
+	//	====	ListByPrefix(prefix string) ([]string, error)
 
-		====		List() ([]string, error)
-	*/
+	// ListByRegex returns a slice of strings representing the base names of the files found in Location that
+	// matched the given regular expression. An empty slice will be returned even for locations that don't exist.
+	//
+	//	====	ListByRegex(regex *regexp.Regexp) ([]string, error)
 
-	/*
-				ListByPrefix returns a slice of strings representing the base names of the files found in Location whose
-				filenames match the given prefix. An empty slice will be returned even for locations that don't exist.
+	// Path returns absolute path to the Location with leading and trailing slashes, ie /some/path/to/
+	//	==== Path() string
 
-		====	ListByPrefix(prefix string) ([]string, error)
-	*/
+	//TODO:  NOT yet the current wording depending on whether we change the exists check (from checking bucket exists)
+	//
+	// Exists returns boolean if the location exists on the file system. Also returns an error if any.
+	//
+	//	For some keystore filesystems, a prefix/folder can't exist if there isn't an object with a key that contains that
+	//	"location", so checking if a given "location" exists is effectively checking if keys with a particular prefix
+	//	"directory" exist.
+	//
+	//	==== Exists() (bool, error)
 
-	/*
-				ListByRegex returns a slice of strings representing the base names of the files found in Location that
-				matched the given regular expression. An empty slice will be returned even for locations that don't exist.
+	// NewLocation is an initializer for a new Location relative to the existing one.
+	//
+	// * "relativePath" parameter may use dot (. and ..) paths and may not begin with a separator character but must
+	// end with a separator character.
+	//
+	// For location:
+	//     file:///some/path/to/
+	// calling:
+	//     NewLocation("../../")
+	// would return a new vfs.Location representing:
+	//     file:///some/
+	//	====	NewLocation(relativePath string) (Location, error)
 
-		====	ListByRegex(regex *regexp.Regexp) ([]string, error)
-	*/
+	// ChangeDir updates the existing Location's path to the provided relative path. For instance, for location:
+	// file:///some/path/to/, calling ChangeDir("../../") update the location instance to file:///some/.
+	//
+	// relativePath may use dot (. and ..) paths and may not begin with a separator character but must end with
+	// a separator character.
+	//   ie., path/to/location, path/to/location/, ./path/to/location, and ./path/to/location/ are all effectively equal.
+	//
+	//	====	ChangeDir(relativePath string) error
 
-	/*
-		Returns the volume as string.  Some filesystems may not have a volume and will return "".  In URI parlance,
-		volume equates to authority.  For example s3://mybucket/path/to/file.txt, volume would return "mybucket".
+	// NewFile will instantiate a vfs.File instance at or relative to the current location's path.
+	//
+	//   * fileName param may use dot (. and ..) paths and may not begin or end with a separator character.
+	//   * Resultant File path will be the shortest path name equivalent of combining the Location path and relative path, if any.
+	//       ie, /tmp/dir1/ as location and fileName "newdir/./../newerdir/file.txt"
+	//       results in /tmp/dir1/newerdir/file.txt for the final vfs.File path.
+	//   * The file may or may not already exist.
+	//   * Upon success, a vfs.File, representing the file's new path (location path + file relative path), will be returned.
+	//   * In the case of an error, nil is returned for the file.
+	//
+	//	====	NewFile(fileName string) (File, error)
 
-		Volume() string
-	*/
+	// DeleteFile deletes the file of the given name at the location. This is meant to be a short cut for
+	// instantiating a new file and calling delete on that, with all the necessary error handling overhead.
+	//
+	// fileName may be a relative path to a file but, as a file, may not end with a separator charactier
+	//   ie., path/to/file.txt, ../../other/path/to/file.text are acceptable but path/to/file.txt/ is not
+	//
+	//	====	DeleteFile(fileName string) error
 
-	/*
-		Path returns absolute path to the Location with leading and trailing slashes, ie /some/path/to/
-		paths's for locations will always end with a separator character.
-
-		Path() string
-	*/
-
-	/*
-		Exists returns boolean if the location exists. Also returns an error if any.
-
-		For some keystore filesystems, a prefix/folder can't exist if there isn't an object with a key that contains that
-		"location", so checking if a given "location" exists is effectively checking if keys with a particular prefix exist.
-
-		Exists() (bool, error)
-	*/
-
-	/*
-				NewLocation is an initializer for a new Location relative to the existing one. For instance, for location:
-				file://some/path/to/, calling NewLocation("../../") would return a new vfs.Location representing file://some/.
-				The new location instance should be on the same file system volume as the location it originated from.
-
-				relativePath may use dot (. and ..) paths and may not begin with a separator character but may or may not end with
-				as separator character.
-				 ie., path/to/location, path/to/location/, ./path/to/location, and ./path/to/location/ are all effectively equal.
-
-		====	NewLocation(relativePath string) (Location, error)
-	*/
-
-	/*
-				ChangeDir updates the existing Location's path to the provided relative path. For instance, for location:
-				file://some/path/to/, calling ChangeDir("../../") update the location instance to file://some/.
-
-				relativePath may use dot (. and ..) paths and may not begin with a separator character but may or may not end with
-				as separator character.
-				 ie., path/to/location, path/to/location/, ./path/to/location, and ./path/to/location/ are all effectively equal.
-
-		====	ChangeDir(relativePath string) error
-	*/
-
-	/*
-				FileSystem returns the underlying vfs.FileSystem struct for Location.
-
-		    	FileSystem() FileSystem
-	*/
-
-	/*
-				NewFile will instantiate a vfs.File instance at the current location's path. In the case of an error,
-				nil will be returned.
-
-				fileName may be a relative path to a file but, as a file, may not end with a separator charactier
-				  ie., path/to/file.txt, ../../other/path/to/file.text are acceptable but path/to/file.txt/ is not
-
-		====	NewFile(fileName string) (File, error)
-	*/
-
-	/*	DeleteFile deletes the file of the given name at the location. This is meant to be a short cut for
-				instantiating a new file and calling delete on that, with all the necessary error handling overhead.
-
-				fileName may be a relative path to a file but, as a file, may not end with a separator charactier
-				  ie., path/to/file.txt, ../../other/path/to/file.text are acceptable but path/to/file.txt/ is not
-
-		====	DeleteFile(fileName string) error
-	*/
-
-	/*
-		URI returns the fully qualified URI for the Location.  IE, s3://bucket/some/path/
-
-		URI's for locations will always end with a separator character.
-
-		URI() string
-	*/
+    // URI returns the fully qualified URI for the Location.  IE, s3://bucket/some/path/
+    //
+	// URI's for locations must always end with a separator character.
 }
 
 //Test File
@@ -521,9 +486,13 @@ func (s *vfsTestSuite) File(baseLoc vfs.Location) {
 		s.NoError(err)
 
 		/*
-			MoveToLocation will move the current file to the provided location. If the file already exists at the location,
-			the contents will be overwritten with the current file's contents. If the location does not exist, an attempt will
-			be made to create it.  In the case of an error, nil is returned for the file.
+			MoveToLocation will move the current file to the provided location.
+
+			* If the file already exists at the location, the contents will be overwritten with the current file's contents.
+			* If the location does not exist, an attempt will be made to create it.
+			* Upon success, a vfs.File, representing the file at the new location, will be returned.
+			* In the case of an error, nil is returned for the file.
+			* When moving within the same Scheme, native move/rename should be used where possible
 
 			MoveToLocation(location Location) (File, error)
 		*/
