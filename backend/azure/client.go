307e8d249d0f7a9801232918bb9c509dd72723d6
Dusty McIlvain <dustin.mcilvain@c2fo.com>
2021-04-28 08:59:30 -0500

    Address PR comments

--- backend/azure/client.go
+++ backend/azure/client.go
@@ -18,12 +18,28 @@ import (
 // The Client interface contains methods that perform specific operations to Azure Blob Storage.  This interface is
 // here so we can write mocks over the actual functionality.
 type Client interface {
+	// Properties should return a BlobProperties struct for the blob specified by locationURI, and filePath.  If the
+	// blob is not found an error should be returned.
 	Properties(locationURI, filePath string) (*BlobProperties, error)
+
+	// SetMetadata should add the metadata specified by the parameter metadata for the blob specified by the parameter
+	// file.
 	SetMetadata(file vfs.File, metadata map[string]string) error
+
+	// Upload should create or update the blob specified by the file parameter with the contents of the content
+	// parameter
 	Upload(file vfs.File, content io.ReadSeeker) error
+
+	// Download should return a reader for the blob specified by the file parameter
 	Download(file vfs.File) (io.ReadCloser, error)
+
+	// Copy should copy the file specified by srcFile to the file specified by tgtFile
 	Copy(srcFile vfs.File, tgtFile vfs.File) error
+
+	// List should return a listing for the specified location.  Listings should include the full path for the file.
 	List(l vfs.Location) ([]string, error)
+
+	// Delete should delete the file specified by the parameter file.
 	Delete(file vfs.File) error
 }
 
