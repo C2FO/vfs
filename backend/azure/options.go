ed2762de9d77bb27a28b3c3cf6de9e70f7ad2b87
Dusty McIlvain <dustin.mcilvain@c2fo.com>
2021-05-07 08:01:27 -0500

    Add missing comment blocks to Options methods

--- backend/azure/options.go
+++ backend/azure/options.go
@@ -10,22 +10,28 @@ import (
 
 // Options contains options necessary for the azure vfs implementation
 type Options struct {
-	// AccountName holds the Azure Blob Storage account name for authentication
+	// AccountName holds the Azure Blob Storage account name for authentication.  This field is required for all
+	// authentication types.
 	AccountName string
 
-	// AccountKey holds the Azure Blob Storage account key for authentication
+	// AccountKey holds the Azure Blob Storage account key for authentication.  This field is used for shared key
+	// authentication.
 	AccountKey string
 
-	// TenantID holds the Azure Service Account tenant id for authentication
+	// TenantID holds the Azure Service Account tenant id for authentication.  This field is used for OAuth token
+	// based authentication.
 	TenantID string
 
-	// ClientID holds the Azure Service Account client id for authentication
+	// ClientID holds the Azure Service Account client id for authentication.  This field is used for OAuth token
+	// based authentication.
 	ClientID string
 
-	// ClientSecret holds the Azure Service Account client secret for authentication
+	// ClientSecret holds the Azure Service Account client secret for authentication.  This field is used for OAuth token
+	// based authentication.
 	ClientSecret string
 
-	// AzureEnvName holds the name for the Azure environment
+	// AzureEnvName holds the name for the Azure environment.  This field is used for OAuth token
+	// based authentication.
 	AzureEnvName string
 
 	// RetryFunc holds the retry function
@@ -34,6 +40,14 @@ type Options struct {
 	tokenCredentialFactory TokenCredentialFactory
 }
 
+// NewOptions creates a new Options struct by populating values from environment variables.
+//   Env Vars:
+//     *VFS_AZURE_STORAGE_ACCOUNT
+//     *VFS_AZURE_STORAGE_ACCESS_KEY
+//     *VFS_AZURE_TENANT_ID
+//     *VFS_AZURE_CLIENT_ID
+//     *VFS_AZURE_CLIENT_SECRET
+//     *VFS_AZURE_ENV_NAME
 func NewOptions() *Options {
 	return &Options{
 		AccountName:            os.Getenv("VFS_AZURE_STORAGE_ACCOUNT"),
@@ -46,6 +60,13 @@ func NewOptions() *Options {
 	}
 }
 
+// The Credential method returns an azblob.Credential struct based on how options are configured.  Options are checked
+// and evaluated in the following order:
+//    1. If TenantID, ClientID, and ClientSecret are non-empty, return azblob.TokenCredential.  This form of authentication
+//       is used with service accounts and can be used to access containers across multiple storage accounts.
+//    2. If AccountName, and AccountKey are non-empty, return azblob.SharedKeyCredential.  This form or authentication
+//       is used with storage accounts and only provides access to a single storage account.
+//    3. Returns an anonymous credential.  This allows access only to public blobs.
 func (o *Options) Credential() (azblob.Credential, error) {
 	// Check to see if we have service account credentials
 	if o.TenantID != "" && o.ClientID != "" && o.ClientSecret != "" {
