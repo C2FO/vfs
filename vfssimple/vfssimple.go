c8bd241e15f206089fe51127bea19f356b519d4f
John Judd <john.judd@c2fo.com>
2021-11-28 15:11:20 -0600

    fix vfssimple registered backend factory to ensure backend returned is the most specific match

--- vfssimple/vfssimple.go
+++ vfssimple/vfssimple.go
@@ -1,10 +1,9 @@
 package vfssimple
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/c2fo/vfs/v6"
@@ -13,13 +12,20 @@ import (
 	"github.com/c2fo/vfs/v6/backend/azure"
 )
 
+var (
+	ErrMissingAuthority = errors.New("unable to determine uri authority ([user@]host[:port]) for network-based scheme")
+	ErrMissingScheme    = errors.New("unable to determine uri scheme")
+	ErrRegFsNotFound    = errors.New("no matching registered filesystem found")
+	ErrBlankURI         = errors.New("uri is blank")
+)
+
 // NewLocation is a convenience function that allows for instantiating a location based on a uri string. Any
 // backend file system is supported, though some may require prior configuration. See the docs for
 // specific requirements of each
 func NewLocation(uri string) (vfs.Location, error) {
 	fs, host, path, err := parseSupportedURI(uri)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to create vfs.Location for uri %q: %w", uri, err)
 	}
 
 	return fs.NewLocation(host, path)
@@ -31,78 +37,101 @@ func NewLocation(uri string) (vfs.Location, error) {
 func NewFile(uri string) (vfs.File, error) {
 	fs, host, path, err := parseSupportedURI(uri)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to create vfs.File for uri %q: %w", uri, err)
 	}
 
 	return fs.NewFile(host, path)
 }
 
-func parseSupportedURI(uri string) (vfs.FileSystem, string, string, error) {
-	var err error
+// parseURI attempts to parse a URI and validate that it returns required results
+func parseURI(uri string) (scheme, authority, path string, err error) {
+	// return early if blank uri
+	if uri == "" {
+		err = ErrBlankURI
+		return
+	}
+
+	// parse URI
 	var u *url.URL
 	u, err = url.Parse(uri)
 	if err != nil {
-		return nil, "", "", err
-	}
-	host := u.Host
-	path := u.Path
-	user := u.User
-	if user.String() != "" {
-		host = fmt.Sprintf("%s@%s", user, host)
+		err = fmt.Errorf("unknown url.Parse error: %w", err)
+		return
 	}
 
-	var fs vfs.FileSystem
-	for _, backendScheme := range backend.RegisteredBackends() {
-		// Azure
-		if azure.IsValidURI(u) {
-			host, path, err = azure.ParsePath(u.Path)
-		}
-
-		// Object-level backend
-		if strings.HasPrefix(uri, backendScheme) {
-			fs = backend.Backend(backendScheme)
-			break
-		}
-		// Bucket-level backend
-		volume := fmt.Sprintf("%s://%s/", u.Scheme, u.Host)
-		if volume == backendScheme {
-			fs = backend.Backend(backendScheme)
-			break
-		}
-
-		// Location-level backend
-		if isInPath(u, volume, backendScheme) {
-			fs = backend.Backend(backendScheme)
-			break
-		}
-
-		// Scheme-level backend
-		if u.Scheme == backendScheme {
-			fs = backend.Backend(backendScheme)
-		}
+	// validate schema
+	scheme = u.Scheme
+	if u.Scheme == "" {
+		err = ErrMissingScheme
+		return
 	}
 
-	if fs == nil {
-		err = fmt.Errorf("%s is an unsupported uri scheme", u.Scheme)
+	// validate authority
+	authority = u.Host
+	path = u.Path
+	if azure.IsValidURI(u) {
+		authority, path, err = azure.ParsePath(path)
 	}
 
-	return fs, host, path, err
+	if u.User.String() != "" {
+		authority = fmt.Sprintf("%s@%s", u.User, u.Host)
+	}
+	// network-based schemes require authority, but not file:// or mem://
+	if authority == "" && !(scheme == "file" || scheme == "mem") {
+		return "", "", "", ErrMissingAuthority
+	}
+
+	return
 }
 
-// isInPath will crawl down the provided url's path to see if it matches the registered root value
-// Example:
-//	 url: s3://bucket/root/path/to/file.txt
-//   root: s3://bucket/root/   <== Registered URI for file system.
+// parseSupportedURI checks if URI matches any backend name as prefix, capturing the longest(most specific) match found.
+// For instance, given registered backends with the names:
 //
-//   This would return true because the target url is within the root.
-func isInPath(u *url.URL, volume, root string) bool {
-	path := u.Path
-	paths := strings.Split(path, string(os.PathSeparator))
-	for i := range paths {
-		path := fmt.Sprintf("%s%s/", volume, filepath.Join(paths[0:i]...))
-		if path == root {
-			return true
+// 's3'                         - registered by default
+// 's3://somebucket/'           - perhaps this was registered using AWS access key id x
+// 's3://somebucket/path/'      - and this was registered using AWS access key id y
+// 's3://somebucket/path/a.txt' - and this was registered using AWS access key id z
+// 's3://some'                  - another contrived registered fs for bucket
+//
+// See the expected registered bucket name for each:
+//
+// 's3://somebucket/path/a.txt' - URI: 's3://somebucket/path/a.txt'         (most specific match)
+// 's3://somebucket/path/a.txt' - URI: 's3://somebucket/path/a.txt.tar.gz'  (prefix still matches)
+// 's3//somebucket/path/'       - URI: 's3://somebucket/path/otherfile.txt' (file only matches path-level registered fs)
+// 's3//somebucket/path/'       - URI: 's3://somebucket/path/'              (exact path-elve match)
+// 's3//somebucket/'            - URI: 's3://somebucket/test/file.txt'      (bucket-level match only)
+// 's3//somebucket/'            - URI: 's3://somebucket/test/'              (still bucket-level match only)
+// 's3//somebucket/'            - URI: 's3://somebucket/'                   (exact bucket-level match)
+// 's3//some'                   - URI: 's3://some-other-bucket/'            (bucket-level match)
+// 's3'                         - URI: 's3://other/'                        (scheme-level match, only)
+// 's3'                         - URI: 's3://other/file.txt'                (scheme-level match, only)
+// 's3'                         - URI: 's3://other/path/to/nowhere/'        (scheme-level match, only)
+func parseSupportedURI(uri string) (vfs.FileSystem, string, string, error) {
+	_, authority, path, err := parseURI(uri)
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	var longest string
+	backends := backend.RegisteredBackends()
+	for _, backendName := range backends {
+		if strings.HasPrefix(uri, backendName) {
+			// The first match always becomes the longest
+			if longest == "" {
+				longest = backendName
+				continue
+			}
+
+			// we found a longer (more specific) backend prefix matching URI
+			if len(backendName) > len(longest) {
+				longest = backendName
+			}
 		}
 	}
-	return false
+
+	if longest == "" {
+		err = ErrRegFsNotFound
+	}
+
+	return backend.Backend(longest), authority, path, err
 }
---
+++ vfssimple/vfssimple_test.go
@@ -0,0 +1,369 @@
+package vfssimple
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/c2fo/vfs/v6/backend"
+	"github.com/c2fo/vfs/v6/backend/s3"
+	"github.com/c2fo/vfs/v6/mocks"
+)
+
+func TestVFSSimple(t *testing.T) {
+	suite.Run(t, new(vfssimplesuite))
+}
+
+type vfssimplesuite struct {
+	suite.Suite
+}
+
+func (s *vfssimplesuite) TestParseURI() {
+	tests := []struct {
+		uri, message, scheme, authority, path string
+		err                                   error
+	}{
+		{
+			uri:     "",
+			err:     ErrBlankURI,
+			message: "cannot use an empty uri",
+		},
+		{
+			uri:     "asdf@asdf.com",
+			err:     ErrMissingScheme,
+			message: "email address is not a uri",
+		},
+		{
+			uri:     "1",
+			err:     ErrMissingScheme,
+			message: "integer is not a uri",
+		},
+		{
+			uri:     "host.com/path",
+			err:     ErrMissingScheme,
+			message: "missing scheme",
+		},
+		{
+			uri:     "s3.test.com",
+			err:     ErrMissingScheme,
+			message: "resembles, but is not, a uri",
+		},
+		{
+			uri:     "/some/path/to/file.txt",
+			err:     ErrMissingScheme,
+			message: "path-only is not a uri",
+		},
+		{
+			uri:     "s3://",
+			err:     ErrMissingAuthority,
+			message: "scheme only is not a uri without authority",
+		},
+		{
+			uri:     "\u007f",
+			err:     errors.New("net/url: invalid control character in URL"),
+			message: "invalid char causes parse error",
+		},
+		{
+			uri:       "fake://host.com/path/to/file.txt",
+			err:       nil,
+			message:   "valid uri for fake scheme",
+			scheme:    "fake",
+			authority: "host.com",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "file:///path/to/file.txt",
+			err:       nil,
+			message:   "valid file uri, no authority required",
+			scheme:    "file",
+			authority: "",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "file://c:/path/to/file.txt",
+			err:       nil,
+			message:   "valid file uri with authority(volume)",
+			scheme:    "file",
+			authority: "c:",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "file://c/path/to/file.txt",
+			err:       nil,
+			message:   "valid file uri with authority(volume), no colon",
+			scheme:    "file",
+			authority: "c",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "mem:///path/to/file.txt",
+			err:       nil,
+			message:   "valid mem uri, no authority (namespace) required",
+			scheme:    "mem",
+			authority: "",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "mem://namespace/path/to/file.txt",
+			err:       nil,
+			message:   "valid mem uri with namespace(authority)",
+			scheme:    "mem",
+			authority: "namespace",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "s3://mybucket/path/to/file.txt",
+			err:       nil,
+			message:   "valid s3 uri",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "gs://mybucket/path/to/file.txt",
+			err:       nil,
+			message:   "valid gs uri",
+			scheme:    "gs",
+			authority: "mybucket",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "https://myaccount.blob.core.windows.net/mycontainer/path/to/file.txt",
+			err:       nil,
+			message:   "valid azure uri",
+			scheme:    "https",
+			authority: "mycontainer",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "sftp://user@host.com/path/to/file.txt",
+			err:       nil,
+			message:   "valid sftp uri",
+			scheme:    "sftp",
+			authority: "user@host.com",
+			path:      "/path/to/file.txt",
+		},
+		{
+			uri:       "sftp://user@host.com:22/path/to/file.txt",
+			err:       nil,
+			message:   "valid sftp uri, with port",
+			scheme:    "sftp",
+			authority: "user@host.com:22",
+			path:      "/path/to/file.txt",
+		},
+	}
+
+	for _, test := range tests {
+		scheme, authority, path, err := parseURI(test.uri)
+		if test.err != nil {
+			s.Error(err, test.message)
+			if errors.Is(err, test.err) {
+				s.True(errors.Is(err, test.err), test.message)
+			} else {
+				// this is necessary since we can't recreate sentinel errors from url.Parse() to do errors.Is() comparison
+				s.Contains(err.Error(), test.err.Error(), test.message)
+			}
+		} else {
+			s.NoError(err, test.message)
+			s.Equal(test.scheme, scheme, test.message)
+			s.Equal(test.authority, authority, test.message)
+			s.Equal(test.path, path, test.message)
+		}
+	}
+}
+
+func (s *vfssimplesuite) TestParseSupportedURI() {
+	// register backend fs's that have a mock client injected that we can introspect in tests to ensure we right the right fs back
+	backend.Register("s3://mybucket/", s3.NewFileSystem().WithClient(getS3NamedClientMock("bucket1")))
+	backend.Register("s3://otherbucket/", s3.NewFileSystem().WithClient(getS3NamedClientMock("bucket2")))
+	backend.Register("s3://mybucket/path/", s3.NewFileSystem().WithClient(getS3NamedClientMock("path")))
+	backend.Register("s3://mybucket/path/file.txt", s3.NewFileSystem().WithClient(getS3NamedClientMock("file1")))
+	backend.Register("s3://mybucket/path/file.txt.pgp", s3.NewFileSystem().WithClient(getS3NamedClientMock("file2")))
+
+	tests := []struct {
+		uri, message, scheme, authority, path, regFS string
+		err                                          error
+	}{
+		{
+			uri:       "s3://mybucket/",
+			err:       nil,
+			message:   "registered bucket1",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/",
+			regFS:     "bucket1",
+		},
+		{
+			uri:       "s3://otherbucket/",
+			err:       nil,
+			message:   "registered bucket2",
+			scheme:    "s3",
+			authority: "otherbucket",
+			path:      "/",
+			regFS:     "bucket2",
+		},
+		{
+			uri:       "s3://mybucket/unregistered/path/",
+			err:       nil,
+			message:   "registered bucket, unregistered path",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/unregistered/path/",
+			regFS:     "bucket1",
+		},
+		{
+			uri:       "s3://mybucket/path/",
+			err:       nil,
+			message:   "registered bucket, registered path",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/path/",
+			regFS:     "path",
+		},
+		{
+			uri:       "s3://mybucket/path/and/more/path/",
+			err:       nil,
+			message:   "registered bucket, registered path with more unregistered path",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/path/and/more/path/",
+			regFS:     "path",
+		},
+		{
+			uri:       "s3://mybucket/path/file.txt",
+			err:       nil,
+			message:   "registered bucket, registered path with file1",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/path/file.txt",
+			regFS:     "file1",
+		},
+		{
+			uri:       "s3://mybucket/path/file.txt.tar.gz",
+			err:       nil,
+			message:   "registered bucket, registered path with file1 prefix", // *********** TODO: not totally sure about this test
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/path/file.txt.tar.gz",
+			regFS:     "file1",
+		},
+		{
+			uri:       "s3://mybucket/path/file.txt.pgp",
+			err:       nil,
+			message:   "registered bucket, registered path with file2",
+			scheme:    "s3",
+			authority: "mybucket",
+			path:      "/path/file.txt.pgp",
+			regFS:     "file2",
+		},
+	}
+
+	for _, test := range tests {
+		fs, authority, path, err := parseSupportedURI(test.uri)
+		if test.err != nil {
+			s.Error(err, test.message)
+			if errors.Is(err, test.err) {
+				s.True(errors.Is(err, test.err), test.message)
+			} else {
+				// this is necessary since we can't recreate sentinel errors from url.Parse() to do errors.Is() comparison
+				s.Contains(err.Error(), test.err.Error(), test.message)
+			}
+		} else {
+			s.NoError(err, test.message)
+			s.Equal(test.scheme, fs.Scheme(), test.message)
+			s.Equal(test.authority, authority, test.message)
+			s.Equal(test.path, path, test.message)
+			// check client for named registered mock
+			switch fs.Scheme() {
+			case "s3":
+				s3api, err := fs.(*s3.FileSystem).Client()
+				s.NoError(err, test.message)
+				if c, ok := s3api.(*namedS3ClientMock); ok {
+					s.Equal(c.RegName, test.regFS, test.message)
+				} else {
+					s.Fail("should have returned mock", test.message)
+				}
+			default:
+				s.Fail("we should have a case for returned fs type", test.message)
+			}
+		}
+	}
+}
+func (s *vfssimplesuite) TestNewFile() {
+	backend.Register("s3://filetest/path/", s3.NewFileSystem().WithClient(getS3NamedClientMock("filetest-path")))
+	backend.Register("s3://filetest/", s3.NewFileSystem().WithClient(getS3NamedClientMock("filetest-bucket")))
+
+	// success
+	goodURI := "s3://filetest/path/file.txt"
+	file, err := NewFile(goodURI)
+	s.NoError(err, "no error expected for NewFile")
+	s.IsType(file, &s3.File{}, "should be an s3.File")
+	s.Equal(file.URI(), goodURI)
+	s3api, err := file.Location().FileSystem().(*s3.FileSystem).Client()
+	s.NoError(err, "no error expected")
+	if c, ok := s3api.(*namedS3ClientMock); ok {
+		s.Equal(c.RegName, "filetest-path", "should be 'filetest-path', not 'filetest-bucket' or 's3'")
+	} else {
+		s.Fail("should have returned mock", "should not reach this")
+	}
+
+	// failure
+	badURI := "unknown://filetest/path/file.txt"
+	file, err = NewFile(badURI)
+	s.Error(err, "error expected for NewFile")
+	s.Nil(file, "file should be nil")
+	s.True(errors.Is(err, ErrRegFsNotFound))
+
+	badURI = ""
+	file, err = NewFile(badURI)
+	s.Error(err, "error expected for NewFile")
+	s.Nil(file, "file should be nil")
+	s.True(errors.Is(err, ErrBlankURI))
+}
+
+func (s *vfssimplesuite) TestNewLocation() {
+	backend.Register("s3://loctest/path/", s3.NewFileSystem().WithClient(getS3NamedClientMock("loctest-path")))
+	backend.Register("s3://loctest/", s3.NewFileSystem().WithClient(getS3NamedClientMock("loctest-bucket")))
+
+	// success
+	goodURI := "s3://loctest/path/to/here/"
+	loc, err := NewLocation(goodURI)
+	s.NoError(err, "no error expected for NewLocation")
+	s.IsType(loc, &s3.Location{}, "should be an s3.Location")
+	s.Equal(loc.URI(), goodURI)
+	s3api, err := loc.FileSystem().(*s3.FileSystem).Client()
+	s.NoError(err, "no error expected")
+	if c, ok := s3api.(*namedS3ClientMock); ok {
+		s.Equal(c.RegName, "loctest-path", "should be 'loctest-path', not 'loctest-bucket' or 's3'")
+	} else {
+		s.Fail("should have returned mock", "should not reach this")
+	}
+
+	// failure
+	badURI := "unknown://filetest/path/to/here/"
+	loc, err = NewLocation(badURI)
+	s.Error(err, "error expected for NewLocation")
+	s.Nil(loc, "location should be nil")
+	s.True(errors.Is(err, ErrRegFsNotFound))
+
+	badURI = ""
+	loc, err = NewLocation(badURI)
+	s.Error(err, "error expected for NewLocation")
+	s.Nil(loc, "location should be nil")
+	s.True(errors.Is(err, ErrBlankURI))
+}
+
+type namedS3ClientMock struct {
+	*mocks.S3API
+	RegName string
+}
+
+// getS3NamedClientMock returns an s3 client that satisfies the interface but we only really care about the name,
+// to introspect in the test return
+func getS3NamedClientMock(name string) *namedS3ClientMock {
+	return &namedS3ClientMock{
+		S3API:   &mocks.S3API{},
+		RegName: name,
+	}
+}
