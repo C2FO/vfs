6bde3b81feda5219229324dd9c0ccc3a06a66a30
Pooja Dhondge <pooja.dhondge@c2fo.com>
2022-06-27 11:25:51 -0500

    add go doc

--- options/options.go
+++ options/options.go
@@ -1,5 +1,16 @@
 package options
 
+// DeleteOption interface contains function that should be implemented by any custom option to qualify as a delete option.
+// Example:
+// ```
+//	type TakeBackupDeleteOption{}
+// 	func (o TakeBackupDeleteOption) DeleteOptionName() string {
+//		return "take backup"
+//	}
+//	func (o TakeBackupDeleteOption) BackupLocation() string {
+// 		return o.backupLocation
+//	}
+// ```
 type DeleteOption interface {
 	DeleteOptionName() string
 }
