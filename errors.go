ca1e8e56014cb9d5a2442ca5a868b79c3c9ea628
John Judd <john.judd@c2fo.com>
2023-11-23 01:20:31 -0600

    add comments to sentinel errors

--- errors.go
+++ errors.go
@@ -6,8 +6,16 @@ type Error string
 // Error returns a string representation of the error
 func (e Error) Error() string { return string(e) }
 
-// CopyToNotPossible - CopyTo/MoveTo operations are only possible when seek position is 0,0
-const CopyToNotPossible = Error("current cursor offset is not 0 as required for this operation")
-const ErrNotExist = Error("file does not exist")
-const ErrSeekInvalidOffset = Error("seek: invalid offset")
-const ErrSeekInvalidWhence = Error("seek: invalid whence")
+const (
+	// CopyToNotPossible - CopyTo/MoveTo operations are only possible when seek position is 0,0
+	CopyToNotPossible = Error("current cursor offset is not 0 as required for this operation")
+
+	// ErrNotExist - File does not exist
+	ErrNotExist = Error("file does not exist")
+
+	// ErrSeekInvalidOffset - Offset is invalid. Must be greater than or equal to 0
+	ErrSeekInvalidOffset = Error("seek: invalid offset")
+
+	// ErrSeekInvalidWhence - Whence is invalid.  Must be one of the following: 0 (io.SeekStart), 1 (io.SeekCurrent), or 2 (io.SeekEnd)
+	ErrSeekInvalidWhence = Error("seek: invalid whence")
+)
