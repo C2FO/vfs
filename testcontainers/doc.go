58067aeba5886910bc5dc11c95abaccd48a16098
Nathan Baulch <nathan.baulch@gmail.com>
2025-01-19 12:01:37 +1100

    Integration testing with testcontainers

---
+++ testcontainers/doc.go
@@ -0,0 +1,5 @@
+/*
+Package testcontainers is meant to be run by implementors of backends to ensure that the behaviors of their backend matches the
+expected behavior of the interface.  It uses the local Docker daemon to run servers that emulate popular storage services.
+*/
+package testcontainers
